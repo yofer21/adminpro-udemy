6d652dc1d82de7ab092b9ee64323dd7cd2a06dc7
Fernando Garcia <agarcia@vates.org>
2018-01-07 19:41:36 -0300

    Fin de la seccioón 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { APP_ROUTES } from './app.routes';
 // Modulos
 import { PagesModule } from './pages/pages.module';
 
+// Servicios
+import { ServiceModule } from './services/service.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, APP_ROUTES, PagesModule, FormsModule],
+  imports: [BrowserModule, APP_ROUTES, PagesModule, FormsModule, ServiceModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
+  imports: [RouterModule, CommonModule],
   declarations: [
     HeaderComponent,
     SidebarComponent,
